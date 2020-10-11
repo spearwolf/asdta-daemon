3cab36853d14129fa02b845e9b9f2bc58402722d
Wolfger Schramm <wolfger@spearwolf.de>
2020-10-11 15:52:36 +0200

    query processes

--- asdta-cli.mjs
+++ asdta-cli.mjs
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
+
+/* eslint-env node */
 import fs from 'fs';
 import os from 'os';
+
 import path from 'path';
+
+import query_processes from './lib/query_processes.mjs';
 
 const home = os.homedir();
 const workspacePath = path.join(home, '.asdta');
-const configPath = path.join(workspacePath, 'asdta-cfg.json');
+
+const [, , ...args] = process.argv;
+
+const configPath =
+  args.length === 1 ? args[0] : path.join(workspacePath, 'asdta-cfg.json');
 
 // =====================
 // 1. load configuration
@@ -13,4 +22,8 @@ const configPath = path.join(workspacePath, 'asdta-cfg.json');
 
 const cfg = JSON.parse(fs.readFileSync(configPath));
 
-console.log('ASDTA CONFIG', JSON.stringify(cfg, null, 2));
+// ==================
+// 2. query processes
+// ==================
+
+query_processes(cfg).then(console.log);
