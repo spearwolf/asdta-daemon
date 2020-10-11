835c02c5b469f49e4754505c8f3e03cf1f363e4b
Wolfger Schramm <wolfger@spearwolf.de>
2020-10-11 18:51:13 +0200

    wip: check and save day states

--- asdta-cli.mjs
+++ asdta-cli.mjs
@@ -6,12 +6,15 @@ import os from 'os';
 
 import path from 'path';
 
+import check_day_states from './lib/check_day_states.mjs';
+import get_day_path from './lib/get_day_path.mjs';
 import load_day_states from './lib/load_day_states.mjs';
 import query_processes from './lib/query_processes.mjs';
 import save_day_states from './lib/save_day_states.mjs';
 
 const home = os.homedir();
 const workspacePath = path.join(home, '.asdta');
+const dayPath = get_day_path(workspacePath);
 
 const [, , ...args] = process.argv;
 
@@ -22,6 +25,7 @@ const configPath =
 const cfg = JSON.parse(fs.readFileSync(configPath));
 
 query_processes(cfg)
-  .then((processes) => load_day_states(processes, workspacePath))
-  .then(save_day_states)
+  .then((processes) => load_day_states(processes, dayPath))
+  .then((processes) => check_day_states(processes, cfg))
+  .then((processes) => save_day_states(processes, dayPath))
   .then(console.log);
